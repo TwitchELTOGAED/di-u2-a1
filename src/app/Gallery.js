d15c2766f062f99c170a0211c27997165dcabed2
Antonio Jurado <antoniojuradoferreira@gmail.com>
2025-10-21 13:18:41 +0200

    Gallery_SinProfile.js

--- src/app/Gallery.js
+++ src/app/Gallery.js
@@ -1,14 +1,4 @@
-// ¡Muéveme a Profile.js!
-export function Profile() {
-    return (
-      <img
-        src="https://i.imgur.com/QIrZWGIs.jpg"
-        alt="Alan L. Hart"
-      />
-    );
-  }
-  
-  export default function Gallery() {
+export default function Gallery() {
     return (
       <section>
         <h1>Científicos increíbles</h1>
